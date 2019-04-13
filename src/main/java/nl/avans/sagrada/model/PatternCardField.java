36e424df1b4690ff99da07b7db4311ee26f9aad9
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-13 17:27:52 +0200

    Added missing attributes

--- src/main/java/nl/avans/sagrada/model/PatternCardField.java
+++ src/main/java/nl/avans/sagrada/model/PatternCardField.java
@@ -2,6 +2,10 @@ package nl.avans.sagrada.model;
 
 public class PatternCardField {
 	private GameDie die;
+	private int positionX;
+	private int positionY;
+	private String color;
+	private int value;
 	
 	public boolean canPlaceDieOnField(GameDie die) {
 		return(false);
