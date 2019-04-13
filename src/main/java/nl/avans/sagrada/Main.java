2440488e940d923fb485ad0b3a5b634f44de2908
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-13 15:31:55 +0200

    Added launch of our GUI

--- src/main/java/nl/avans/sagrada/Main.java
+++ src/main/java/nl/avans/sagrada/Main.java
@@ -1,6 +1,28 @@
 package nl.avans.sagrada;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-    }
+public class Main extends Application {
+	public static final int SCREEN_HEIGHT = 800;
+	public static final int SCREEN_WIDTH = 1280;
+	private MyScene rootScene;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setHeight(SCREEN_HEIGHT);
+		primaryStage.setWidth(SCREEN_WIDTH);
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Sagrada groep P");
+		buildGui(primaryStage);
+		primaryStage.show();
+	}
+	private void buildGui(Stage primaryStage) {
+		rootScene = new MyScene();
+		primaryStage.setScene(rootScene);
+	}
+
 }
