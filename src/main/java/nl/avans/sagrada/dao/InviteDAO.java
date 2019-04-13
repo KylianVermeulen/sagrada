14728e407b60a1de7987e44b57d285059b4abba8
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-13 20:00:31 +0200

    Added invite model and dao

---
+++ src/main/java/nl/avans/sagrada/dao/InviteDAO.java
@@ -0,0 +1,19 @@
+package nl.avans.sagrada.dao;
+
+import java.util.ArrayList;
+
+import nl.avans.sagrada.database.DBConnection;
+import nl.avans.sagrada.model.Account;
+import nl.avans.sagrada.model.Invite;
+
+public class InviteDAO {
+	private DBConnection dbConnection;
+	
+	public ArrayList<Invite> getInvitesOfAccount(Account account) {
+		return(null);
+	}
+	
+	public void addInvite(Invite invite) {
+		
+	}
+}
