04d408c426d1c2a5c98311e420dd25bdaaad93d5
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-13 17:39:47 +0200

    Removed methodes

--- src/main/java/nl/avans/sagrada/model/Account.java
+++ src/main/java/nl/avans/sagrada/model/Account.java
@@ -6,24 +6,4 @@ public class Account {
 	private String username;
 	private String password;
 	private ArrayList<Player> players;
-	
-	private String hashPassword(String password) {
-		return(password);
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getPassword() {
-		return(password);
-	}
-	
-	public String getUsername() {
-		return(username);
-	}
 }
