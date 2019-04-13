8b36bb4a79a6b466081b518af0fdc29e09d32113
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-13 14:24:34 +0200

    Added controllers

---
+++ src/main/java/controller/PlayerController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import nl.avans.sagrada.model.GameDie;
+import nl.avans.sagrada.model.PatterncardField;
+import nl.avans.sagrada.model.Player;
+import nl.avans.sagrada.model.Toolcard;
+
+public class PlayerController {
+	private Player player;
+	
+	public void seeToolcards() {
+		
+	}
+	public void seeToolcard() {
+		
+	}
+	
+	public void overviewOfGame() {
+		
+	}
+	
+	public void seePatterncardOfPlayer(Player player) {
+		
+	}
+	
+	public void useToolcard(Toolcard toolcard) {
+		
+	}
+	
+	public void toggleCheatmode() {
+		
+	}
+	
+	public void placeDie(GameDie die, PatterncardField patterncardField) {
+		
+	}
+	
+	public void leaveGame() {
+		
+	}
+}
