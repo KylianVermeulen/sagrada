67460ae0d966f236c2220c3c193287b7665bd20d
LeonNiekus <leonniekusnl@gmail.com>
2019-04-14 13:37:53 +0200

    wip

--- src/main/java/nl/avans/sagrada/dao/AccountDAO.java
+++ src/main/java/nl/avans/sagrada/dao/AccountDAO.java
@@ -1,21 +1,45 @@
 package nl.avans.sagrada.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import nl.avans.sagrada.database.DBConnection;
+import nl.avans.sagrada.database.Query;
+import nl.avans.sagrada.database.QueryParameter;
 import nl.avans.sagrada.model.Account;
 
 public class AccountDAO {
-    private DBConnection dbConnection;
-
-    public Account getAccountByUsername(String username) {
+	private DBConnection dbConnection;
+	
+	
+    public Account mapRowOne(ResultSet rs) throws SQLException {
+        Account account = new Account();
+        if (rs.next()) {
+            account.setUsername(rs.getString("username"));
+            account.setPassword(rs.getString("password"));
+        }
+        return account;
+    }
+    
+	public Account getAccountByUsername(String username) {
+        try {
+            ResultSet rs = dbConnection.executeQuery(
+                    new Query("select * from account where username=?", "query",
+                            new QueryParameter(QueryParameter.STRING, username)));
+            Account account = mapRowOne(rs);
+            return account;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
-    }
-
-    public void updateAccount(Account account) {
-
-    }
-
-    public void addAccount(Account account) {
-
-    }
+	}
+	
+	public void updateAccount(Account account) {
+		
+	}
+	
+	public void addAccount(Account account) {
+		
+	}
 
 }
