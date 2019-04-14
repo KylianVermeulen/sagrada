0aa58773f3d571d6741ea007d45527640891f9fc
LeonNiekus <leonniekusnl@gmail.com>
2019-04-14 13:37:53 +0200

    wip

--- src/main/java/nl/avans/sagrada/model/Account.java
+++ src/main/java/nl/avans/sagrada/model/Account.java
@@ -6,4 +6,21 @@ public class Account {
 	private String username;
 	private String password;
 	private ArrayList<Player> players;
+
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
+
+
