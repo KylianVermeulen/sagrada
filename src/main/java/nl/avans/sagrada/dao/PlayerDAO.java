8b7596411cccf5be55c897a8b4e3382973880691
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-15 14:06:39 +0200

    Added method to get player by id

--- src/main/java/nl/avans/sagrada/dao/PlayerDAO.java
+++ src/main/java/nl/avans/sagrada/dao/PlayerDAO.java
@@ -1,19 +1,44 @@
 package nl.avans.sagrada.dao;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import nl.avans.sagrada.database.DBConnection;
+import nl.avans.sagrada.database.Query;
+import nl.avans.sagrada.database.QueryParameter;
 import nl.avans.sagrada.model.Account;
 import nl.avans.sagrada.model.Game;
 import nl.avans.sagrada.model.Player;
 
 public class PlayerDAO {
     private DBConnection dbConnection;
+    
+    public PlayerDAO() {
+        dbConnection = new DBConnection();
+    }
 
     public ArrayList<Player> getPlayersOfAccount(Account account) {
         return null;
     }
     public void createNewPlayer(Player player, Game game) {
 
+    }
+    
+    public Player getPlayerById(int id) {
+        Player player = new Player();
+        try {
+            ResultSet rs = dbConnection.executeQuery(
+                    new Query("SELECT * FROM player WHERE idplayer=?", "query",
+                            new QueryParameter(QueryParameter.INT, id)));
+            if (rs.next()) {
+                player.setId(rs.getInt("idplayer"));
+                player.setPlayerStatus(rs.getString("playstatus_playstatus"));
+                player.setSeqnr(rs.getInt("seqnr"));
+                player.setIsCurrentPlayer(rs.getBoolean("isCurrentPlayer"));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return player;
     }
 }
