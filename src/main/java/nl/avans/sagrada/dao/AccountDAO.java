1538ac143a79efe466ad0560c5501dbb460ef543
Peter Romijn <peter@samebestdevelopment.nl>
2019-04-15 14:04:00 +0200

    Added method to get account by username

--- src/main/java/nl/avans/sagrada/dao/AccountDAO.java
+++ src/main/java/nl/avans/sagrada/dao/AccountDAO.java
@@ -1,13 +1,36 @@
 package nl.avans.sagrada.dao;
 
+import java.sql.ResultSet;
+
 import nl.avans.sagrada.database.DBConnection;
+import nl.avans.sagrada.database.Query;
+import nl.avans.sagrada.database.QueryParameter;
 import nl.avans.sagrada.model.Account;
 
 public class AccountDAO {
     private DBConnection dbConnection;
+    
+    public AccountDAO() {
+        dbConnection = new DBConnection();
+    }
 
     public Account getAccountByUsername(String username) {
-        return null;
+        Account account = null;
+        try {
+            ResultSet rs = dbConnection.executeQuery(
+                    new Query("SELECT * FROM account WHERE username=?", "query",
+                            new QueryParameter(QueryParameter.STRING, username)
+                    )
+            );
+            if (rs.next()) {
+                String accountUsername = rs.getString("username");
+                String accountPassword = rs.getString("password");
+                account = new Account(accountUsername, accountPassword);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return account;
     }
 
     public void updateAccount(Account account) {
