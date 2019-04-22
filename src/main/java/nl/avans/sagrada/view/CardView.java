dfef33b7d6fc0aba3835862c3356509e38a795fb
Tim Kaerts <t.kaerts@hotmail.nl>
2019-04-22 13:18:36 +0200

    toolcardview (needs more work)

--- src/main/java/nl/avans/sagrada/view/CardView.java
+++ src/main/java/nl/avans/sagrada/view/CardView.java
@@ -1,5 +1,41 @@
 package nl.avans.sagrada.view;
 
-public abstract class CardView {
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import nl.avans.sagrada.view.interfaces.ViewInterface;
 
+public abstract class CardView extends BorderPane implements ViewInterface {
+    private static final int WIDTH = 310;
+    private static final int HEIGHT = 440;
+    private static final Color CARDSANDYELLOW = Color.rgb(255, 240, 173);
+    
+    /**
+     * Empty constructor
+     */
+    public CardView() {
+        super();
+        setPrefSize(WIDTH, HEIGHT);
+        setBackground(new Background(new BackgroundFill(CARDSANDYELLOW, null, null)));
+        String css = this.getClass().getResource("/css/style.css").toExternalForm();
+        getStylesheets().add(css);
+        setId("CardView");
+    }
+    
+    /**
+     * Returns the standard height for cards.
+     * @return standard card height (int)
+     */
+    public int getViewHeight() {
+        return HEIGHT;
+    }
+    
+    /**
+     * Returns the standard width for cards.
+     * @return standard card width (int)
+     */
+    public int getViewWidth() {
+        return WIDTH;
+    }
 }
--- src/main/java/nl/avans/sagrada/view/PrivateObjectiveCardView.java
+++ src/main/java/nl/avans/sagrada/view/PrivateObjectiveCardView.java
@@ -1,5 +1,13 @@
 package nl.avans.sagrada.view;
 
 public class PrivateObjectiveCardView extends CardView {
+	private PrivateObjectiveCard privateObjectiveCard;
+	
+	
+	@Override
+	public void render() {
+		
+		
+	}
 
 }
