ca5b2fb9b8ef43f990a1d4be1eddb6264664c0a9
LeonNiekus <leonniekusnl@gmail.com>
2019-04-22 22:53:02 +0200

    initial commit

--- src/main/java/nl/avans/sagrada/model/PublicObjectiveCard.java
+++ src/main/java/nl/avans/sagrada/model/PublicObjectiveCard.java
@@ -4,4 +4,12 @@ public class PublicObjectiveCard {
     private int id;
     private int seqnr;
     private String description;
+    
+    public PublicObjectiveCard(int id, int seqnr, String description) {
+        this.id = id;
+        this.seqnr = seqnr;
+        this.description = description;
+    }
+    
+    
 }
--- src/main/java/nl/avans/sagrada/view/PublicObjectiveCardView.java
+++ src/main/java/nl/avans/sagrada/view/PublicObjectiveCardView.java
@@ -1,5 +1,17 @@
 package nl.avans.sagrada.view;
 
-public class PublicObjectiveCardView extends CardView {
+import nl.avans.sagrada.view.interfaces.ViewInterface;
+
+public class PublicObjectiveCardView extends CardView implements ViewInterface {
+
+    public PublicObjectiveCardView() {
+        
+    }
+    
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
