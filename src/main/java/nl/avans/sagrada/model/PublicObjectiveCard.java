5ad831cccdd6abefbc03d46415ceaa5eb5199067
LeonNiekus <leonniekusnl@gmail.com>
2019-04-23 11:42:07 +0200

    WIP visuals

--- src/main/java/nl/avans/sagrada/model/PublicObjectiveCard.java
+++ src/main/java/nl/avans/sagrada/model/PublicObjectiveCard.java
@@ -4,11 +4,45 @@ public class PublicObjectiveCard {
     private int id;
     private int seqnr;
     private String description;
+    private int scorePoints;
     
-    public PublicObjectiveCard(int id, int seqnr, String description) {
+    public PublicObjectiveCard(int id, int seqnr, String description, int scorePoints) {
         this.id = id;
         this.seqnr = seqnr;
         this.description = description;
+        this.scorePoints = scorePoints;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSeqnr() {
+        return seqnr;
+    }
+
+    public void setSeqnr(int seqnr) {
+        this.seqnr = seqnr;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getScorePoints() {
+        return scorePoints;
+    }
+
+    public void setScorePoints(int scorePoints) {
+        this.scorePoints = scorePoints;
     }
     
     
--- src/main/java/nl/avans/sagrada/view/PublicObjectiveCardView.java
+++ src/main/java/nl/avans/sagrada/view/PublicObjectiveCardView.java
@@ -1,10 +1,55 @@
 package nl.avans.sagrada.view;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import nl.avans.sagrada.Main;
+import nl.avans.sagrada.controller.PlayerController;
+import nl.avans.sagrada.model.PublicObjectiveCard;
 import nl.avans.sagrada.view.interfaces.ViewInterface;
 
-public class PublicObjectiveCardView extends CardView implements ViewInterface {
-
-    public PublicObjectiveCardView() {
+public class PublicObjectiveCardView extends CardView {
+    private PublicObjectiveCard publicObjectiveCard;
+    private PlayerController playerController;
+    
+    public PublicObjectiveCardView(PlayerController playerController) {
+        super();
+        this.playerController = playerController;
+    }
+    
+    public PublicObjectiveCard getPublicObjectiveCard() {
+        return publicObjectiveCard;
+    }
+    
+    public void setPublicObjectiveCard(PublicObjectiveCard publicObjectiveCard) {
+        this.publicObjectiveCard = publicObjectiveCard;
+    }
+    
+    public void showImage(String url) {
+        Image image = new Image(url);
+        StackPane imagePane = new StackPane();
+        ImageView imageView = new ImageView();
+        imageView.setFitHeight((getViewHeight() / 1.5));
+        imageView.setFitWidth(getViewWidth() + 1);
+        imageView.setPreserveRatio(false);
+        imagePane.getChildren().add(imageView);
+        imagePane.setPrefSize(getViewWidth(), (getViewHeight()/1.5));
+        setCenter(imagePane);
+    }
+    
+    public void showText() {
+        Text scorePoints = new Text(Integer.toString(publicObjectiveCard.getScorePoints()));
+        Text description = new Text(publicObjectiveCard.getDescription());
+        scorePoints.setFont(Main.SAGRADA_FONT);
+        description.setFont(Main.SAGRADA_FONT);
+        BorderPane textPane = new BorderPane();
+        StackPane scorePane = new StackPane();
+        StackPane descriptionPane = new StackPane();
+        textPane.setLeft(scorePane);
+        textPane.setCenter(descriptionPane);
+        scorePane.setPrefWidth((getViewWidth()/6));
         
     }
     
