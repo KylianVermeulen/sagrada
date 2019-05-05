41f4508346e2bee92dbeab8ecc330554ad003231
Tim Kaerts <t.kaerts@hotmail.nl>
2019-05-05 20:42:32 +0200

    met-geimporteerde-stylesheet

--- src/main/java/nl/avans/sagrada/Main.java
+++ src/main/java/nl/avans/sagrada/Main.java
@@ -7,28 +7,28 @@ import javafx.stage.Stage;
 import nl.avans.sagrada.view.MyScene;
 
 public class Main extends Application {
-    public static final int SCREEN_HEIGHT = 800;
-    public static final int SCREEN_WIDTH = 1280;
-    public static final Font SAGRADA_FONT = new Font("Segoe Script", 8);
-    private MyScene rootScene;
+  public static final int SCREEN_HEIGHT = 800;
+  public static final int SCREEN_WIDTH = 1280;
+  public static final Font SAGRADA_FONT = new Font("Segoe Script", 8);
+  private MyScene rootScene;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setHeight(SCREEN_HEIGHT);
-        primaryStage.setWidth(SCREEN_WIDTH);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("Sagrada groep P");
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
-        buildGui(primaryStage);
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setHeight(SCREEN_HEIGHT);
+    primaryStage.setWidth(SCREEN_WIDTH);
+    primaryStage.setResizable(false);
+    primaryStage.setTitle("Sagrada groep P");
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+    buildGui(primaryStage);
+    primaryStage.show();
+  }
 
-    private void buildGui(Stage primaryStage) {
-        rootScene = new MyScene();
-        primaryStage.setScene(rootScene);
-    }
+  private void buildGui(Stage primaryStage) {
+    rootScene = new MyScene();
+    primaryStage.setScene(rootScene);
+  }
 }
