e6c8a814ba167a4f021744b75a1544a9cc135b26
Tim Kaerts <t.kaerts@hotmail.nl>
2019-05-06 13:37:45 +0200

    with-spacing

--- src/main/java/nl/avans/sagrada/view/CardView.java
+++ src/main/java/nl/avans/sagrada/view/CardView.java
@@ -4,16 +4,16 @@ import javafx.scene.layout.BorderPane;
 import nl.avans.sagrada.view.interfaces.ViewInterface;
 
 public abstract class CardView extends BorderPane implements ViewInterface {
-  public static final int CARD_WIDTH = 130;
-  public static final int CARD_HEIGHT = 170;
+    public static final int CARD_WIDTH = 130;
+    public static final int CARD_HEIGHT = 170;
 
-  /**
-   * Empty constructor
-   */
-  public CardView() {
-    setPrefSize(CARD_WIDTH, CARD_HEIGHT);
-    String css = this.getClass().getResource("/css/style.css").toExternalForm();
-    getStylesheets().add(css);
-    setId("CardView");
-  }
+    /**
+     * Empty constructor
+     */
+    public CardView() {
+        setPrefSize(CARD_WIDTH, CARD_HEIGHT);
+        String css = this.getClass().getResource("/css/style.css").toExternalForm();
+        getStylesheets().add(css);
+        setId("CardView");
+    }
 }
