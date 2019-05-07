ed5f9acddcedb4895172296a1b82cb1cbbdee114
Ianhendriks <ianhendriks@gmail.com>
2019-05-07 14:12:27 +0200

    chat

--- src/main/java/nl/avans/sagrada/controller/PlayerController.java
+++ src/main/java/nl/avans/sagrada/controller/PlayerController.java
@@ -28,169 +28,171 @@ import nl.avans.sagrada.view.popups.Alert;
 import nl.avans.sagrada.view.popups.AlertType;
 
 public class PlayerController {
-	private Player player;
-	private MyScene myScene;
+    private Player player;
+    private MyScene myScene;
 
-	public PlayerController(MyScene myScene) {
-		this.myScene = myScene;
-	}
+    public PlayerController(MyScene myScene) {
+        this.myScene = myScene;
+    }
 
-	public void actionJoinGame(Account account, Game game) {
-		player = new PlayerDao().getPlayerByAccountAndGame(account, game);
-		player.setGame(game);
-		if (player.getPatternCard() == null) {
-			viewOptionalPatternCards();
-		}
-	}
+    public void actionJoinGame(Account account, Game game) {
+        player = new PlayerDao().getPlayerByAccountAndGame(account, game);
+        player.setGame(game);
+        if (player.getPatternCard() == null) {
+            viewOptionalPatternCards();
+        }
+    }
 
-	public void viewOptionalPatternCards() {
-		Pane pane = new Pane();
-		ArrayList<PatternCard> patternCards = new PatternCardDao().getOptionalPatternCardsOfPlayer(player);
-		PatternCardSelectionView patternCardSelectionView = new PatternCardSelectionView(this);
-		patternCardSelectionView.setOptionalPatternCards(patternCards);
-		patternCardSelectionView.render();
-		pane.getChildren().add(patternCardSelectionView);
-		myScene.setContentPane(pane);
-	}
+    public void viewOptionalPatternCards() {
+        Pane pane = new Pane();
+        ArrayList<PatternCard> patternCards =
+                new PatternCardDao().getOptionalPatternCardsOfPlayer(player);
+        PatternCardSelectionView patternCardSelectionView = new PatternCardSelectionView(this);
+        patternCardSelectionView.setOptionalPatternCards(patternCards);
+        patternCardSelectionView.render();
+        pane.getChildren().add(patternCardSelectionView);
+        myScene.setContentPane(pane);
+    }
 
-	public void actionSelectPatternCard(PatternCard patternCard) {
-		PlayerDao playerDao = new PlayerDao();
-		player.setPatternCard(patternCard);
-		playerDao.updateSelectedPatternCard(player, patternCard);
-		player.generateFavorTokens();
-		viewPatternCardOfPlayer(player);
-	}
+    public void actionSelectPatternCard(PatternCard patternCard) {
+        PlayerDao playerDao = new PlayerDao();
+        player.setPatternCard(patternCard);
+        playerDao.updateSelectedPatternCard(player, patternCard);
+        player.generateFavorTokens();
+        viewPatternCardOfPlayer(player);
+    }
 
-	/**
-	 * Example code
-	 *
-	 * @param game the game to view the three Toolcards of.
-	 */
-	public void viewToolcards(Game game) {
-		ToolcardDao toolcardDao = new ToolcardDao();
-		BorderPane pane = new BorderPane();
-		ToolCardView[] toolcardViews = new ToolCardView[3];
-		Toolcard[] toolcards = toolcardDao.getToolcardsOfGame(game).toArray(new Toolcard[3]);
-		for (int index = 0; index < toolcardViews.length; index++) {
-			toolcardViews[index] = new ToolCardView(this);
-			toolcardViews[index].setToolCard(toolcards[index]);
-			toolcardViews[index].render();
-		}
+    /**
+     * Example code
+     *
+     * @param game the game to view the three Toolcards of.
+     */
+    public void viewToolcards(Game game) {
+        ToolcardDao toolcardDao = new ToolcardDao();
+        BorderPane pane = new BorderPane();
+        ToolCardView[] toolcardViews = new ToolCardView[3];
+        Toolcard[] toolcards = toolcardDao.getToolcardsOfGame(game).toArray(new Toolcard[3]);
+        for (int index = 0; index < toolcardViews.length; index++) {
+            toolcardViews[index] = new ToolCardView(this);
+            toolcardViews[index].setToolCard(toolcards[index]);
+            toolcardViews[index].render();
+        }
 
-		BorderPane.setMargin(toolcardViews[0], new Insets(0, 5, 0, 0));
-		BorderPane.setMargin(toolcardViews[1], new Insets(0, 5, 0, 5));
-		BorderPane.setMargin(toolcardViews[2], new Insets(0, 0, 0, 5));
-		pane.setLeft(toolcardViews[0]);
-		pane.setCenter(toolcardViews[1]);
-		pane.setRight(toolcardViews[2]);
-		myScene.setContentPane(pane);
-	}
+        BorderPane.setMargin(toolcardViews[0], new Insets(0, 5, 0, 0));
+        BorderPane.setMargin(toolcardViews[1], new Insets(0, 5, 0, 5));
+        BorderPane.setMargin(toolcardViews[2], new Insets(0, 0, 0, 5));
+        pane.setLeft(toolcardViews[0]);
+        pane.setCenter(toolcardViews[1]);
+        pane.setRight(toolcardViews[2]);
+        myScene.setContentPane(pane);
+    }
 
-	/**
-	 * Example code
-	 *
-	 * @param game      the game to view Toolcard of.
-	 * @param selection the selected Toolcard to view.
-	 */
-	public void viewToolcard(Game game, int selection) {
-		ToolcardDao toolcardDao = new ToolcardDao();
+    /**
+     * Example code
+     *
+     * @param game the game to view Toolcard of.
+     * @param selection the selected Toolcard to view.
+     */
+    public void viewToolcard(Game game, int selection) {
+        ToolcardDao toolcardDao = new ToolcardDao();
 
-		Pane pane = new Pane();
-		ToolCardView toolCardView = new ToolCardView(this);
-		toolCardView.setToolCard(toolcardDao.getToolcardsOfGame(game).get(selection));
-		toolCardView.render();
+        Pane pane = new Pane();
+        ToolCardView toolCardView = new ToolCardView(this);
+        toolCardView.setToolCard(toolcardDao.getToolcardsOfGame(game).get(selection));
+        toolCardView.render();
 
-		pane.getChildren().add(toolCardView);
-		myScene.setContentPane(pane);
-	}
+        pane.getChildren().add(toolCardView);
+        myScene.setContentPane(pane);
+    }
 
-	/**
-	 * Example code
-	 *
-	 * @param game Game
-	 */
-	public void viewPublicObjectiveCards(Game game) {
-		BorderPane pane = new BorderPane();
-		PublicObjectiveCardDao publicObjectiveCardDao = new PublicObjectiveCardDao();
-		PublicObjectiveCardView[] publicObjectiveCardViews = new PublicObjectiveCardView[3];
-		PublicObjectiveCard[] publicObjectiveCards = publicObjectiveCardDao.getAllPublicObjectiveCardsOfGame(game)
-				.toArray(new PublicObjectiveCard[3]);
-		for (int index = 0; index < publicObjectiveCardViews.length; index++) {
-			publicObjectiveCardViews[index] = new PublicObjectiveCardView(this);
-			publicObjectiveCardViews[index].setPublicObjectiveCard(publicObjectiveCards[index]);
-			publicObjectiveCardViews[index].render();
-		}
+    /**
+     * Example code
+     *
+     * @param game Game
+     */
+    public void viewPublicObjectiveCards(Game game) {
+        BorderPane pane = new BorderPane();
+        PublicObjectiveCardDao publicObjectiveCardDao = new PublicObjectiveCardDao();
+        PublicObjectiveCardView[] publicObjectiveCardViews = new PublicObjectiveCardView[3];
+        PublicObjectiveCard[] publicObjectiveCards = publicObjectiveCardDao
+                .getAllPublicObjectiveCardsOfGame(game).toArray(new PublicObjectiveCard[3]);
+        for (int index = 0; index < publicObjectiveCardViews.length; index++) {
+            publicObjectiveCardViews[index] = new PublicObjectiveCardView(this);
+            publicObjectiveCardViews[index].setPublicObjectiveCard(publicObjectiveCards[index]);
+            publicObjectiveCardViews[index].render();
+        }
 
-		BorderPane.setMargin(publicObjectiveCardViews[0], new Insets(0, 5, 0, 0));
-		BorderPane.setMargin(publicObjectiveCardViews[1], new Insets(0, 5, 0, 5));
-		BorderPane.setMargin(publicObjectiveCardViews[2], new Insets(0, 0, 0, 5));
-		pane.setLeft(publicObjectiveCardViews[0]);
-		pane.setCenter(publicObjectiveCardViews[1]);
-		pane.setRight(publicObjectiveCardViews[2]);
-		myScene.setContentPane(pane);
-	}
+        BorderPane.setMargin(publicObjectiveCardViews[0], new Insets(0, 5, 0, 0));
+        BorderPane.setMargin(publicObjectiveCardViews[1], new Insets(0, 5, 0, 5));
+        BorderPane.setMargin(publicObjectiveCardViews[2], new Insets(0, 0, 0, 5));
+        pane.setLeft(publicObjectiveCardViews[0]);
+        pane.setCenter(publicObjectiveCardViews[1]);
+        pane.setRight(publicObjectiveCardViews[2]);
+        myScene.setContentPane(pane);
+    }
 
-	/**
-	 * Example code
-	 *
-	 * @param player the player to view the PatternCard of.
-	 */
-	public void viewPatternCardOfPlayer(Player player) {
-		Pane pane = new Pane();
-		PatternCard patternCard = player.getPatternCard();
-		PatternCardView patternCardView = new PatternCardView(this);
-		patternCardView.setPatternCard(patternCard);
-		patternCardView.render();
-		pane.getChildren().add(patternCardView);
-		myScene.setContentPane(pane);
-	}
+    /**
+     * Example code
+     *
+     * @param player the player to view the PatternCard of.
+     */
+    public void viewPatternCardOfPlayer(Player player) {
+        Pane pane = new Pane();
+        PatternCard patternCard = player.getPatternCard();
+        PatternCardView patternCardView = new PatternCardView(this);
+        patternCardView.setPatternCard(patternCard);
+        patternCardView.render();
+        pane.getChildren().add(patternCardView);
+        myScene.setContentPane(pane);
+    }
 
-	/**
-	 * Example code
-	 */
-	public void makeDie() {
-		GameDie gameDie = new GameDie(1, "geel", 6);
-		DieView dieView = new DieView();
-		dieView.setGameDie(gameDie);
-		dieView.render();
+    /**
+     * Example code
+     */
+    public void makeDie() {
+        GameDie gameDie = new GameDie(1, "geel", 6);
+        DieView dieView = new DieView();
+        dieView.setGameDie(gameDie);
+        dieView.render();
 
-		myScene.setContentPane(dieView);
-	}
+        myScene.setContentPane(dieView);
+    }
 
-	/**
-	 * Method that adds a message to the view and database
-	 *
-	 * @param textfield TextField
-	 */
-	public void sendMessage(String text) {
-		ChatlineDao chatlineDao = new ChatlineDao();
-		Chatline chatline = new Chatline(player, text);
-		chatlineDao.getTime(chatline);
+    /**
+     * Method that adds a message to the view and database
+     *
+     * @param textfield TextField
+     */
+    public void sendMessage(String text) {
+        ChatlineDao chatlineDao = new ChatlineDao();
+        Chatline chatline = new Chatline(player, text);
+        chatlineDao.getTime(chatline);
 
-		if (!text.matches("")) {
-			if (chatlineDao.timeExistsOfPlayer(chatline) == false) {
-				chatlineDao.addChatline(chatline);
-				ChatLineView chatview = new ChatLineView(this);
-				chatview.addExistingMessages(player.getChatlines());
-				chatview.addMessage(chatline);
-				player.addChatline(chatline);
-				myScene.setContentPane(chatview);
-			} else {
-				Alert alert = new Alert("Waarschuwing", "Je mag maar 1 keer per seconde een bericht versturen!",
-						AlertType.ERROR);
-				myScene.addAlertPane(alert);
-			}
-		} else {
-			Alert alert = new Alert("Waarschuwing", "Je bericht moet tekst bevatten", AlertType.ERROR);
-			myScene.addAlertPane(alert);
-		}
-	}
+        if (!text.matches("")) {
+            if (chatlineDao.timeExistsOfPlayer(chatline) == false) {
+                chatlineDao.addChatline(chatline);
+                ChatLineView chatview = new ChatLineView(this);
+                chatview.addExistingMessages(player.getChatlines());
+                chatview.addMessage(chatline);
+                player.addChatline(chatline);
+                myScene.setContentPane(chatview);
+            } else {
+                Alert alert = new Alert("Waarschuwing",
+                        "Je mag maar 1 keer per seconde een bericht versturen!", AlertType.ERROR);
+                myScene.addAlertPane(alert);
+            }
+        } else {
+            Alert alert =
+                    new Alert("Waarschuwing", "Je bericht moet tekst bevatten", AlertType.ERROR);
+            myScene.addAlertPane(alert);
+        }
+    }
 
-	/**
-	 * Method to view the chat
-	 */
-	public void viewChat() {
-		ChatLineView chatlineview = new ChatLineView(this);
-		myScene.setContentPane(chatlineview);
-	}
+    /**
+     * Method to view the chat
+     */
+    public void viewChat() {
+        ChatLineView chatlineview = new ChatLineView(this);
+        myScene.setContentPane(chatlineview);
+    }
 }
--- src/main/java/nl/avans/sagrada/model/Chatline.java
+++ src/main/java/nl/avans/sagrada/model/Chatline.java
@@ -6,68 +6,74 @@ public class Chatline {
     private Player player;
     private String message;
     private Timestamp timestamp;
-    
+
     /**
      * Constructor
+     * 
      * @param player Player
      * @param message String
      */
     public Chatline(Player player, String message) {
-    	this.player = player;
-    	this.message = message;
+        this.player = player;
+        this.message = message;
     }
-    
+
     /**
      * Empty Constructor
      */
-    public Chatline() {
-    }
+    public Chatline() {}
 
     /**
      * get player from chatline
+     * 
      * @return
      */
-	public Player getPlayer() {
-		return player;
-	}
+    public Player getPlayer() {
+        return player;
+    }
 
-	/**
-	 * set player for the chatline
-	 * @param player Player
-	 */
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
+    /**
+     * set player for the chatline
+     * 
+     * @param player Player
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 
-	/**
-	 * get message from chatline
-	 * @return
-	 */
-	public String getMessage() {
-		return message;
-	}
+    /**
+     * get message from chatline
+     * 
+     * @return
+     */
+    public String getMessage() {
+        return message;
+    }
 
-	/**
-	 * set message for chatline
-	 * @param message String
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    /**
+     * set message for chatline
+     * 
+     * @param message String
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	/**
-	 * get timestamp from message
-	 * @return
-	 */
-	public Timestamp getTimestamp() {
-		return timestamp;
-	}
+    /**
+     * get timestamp from message
+     * 
+     * @return
+     */
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
 
-	/**
-	 * set timestamp for message
-	 * @param timestamp Timestamp
-	 */
-	public void setTimestamp(Timestamp timestamp) {
-		this.timestamp = timestamp;
-	}
+    /**
+     * set timestamp for message
+     * 
+     * @param timestamp Timestamp
+     */
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
