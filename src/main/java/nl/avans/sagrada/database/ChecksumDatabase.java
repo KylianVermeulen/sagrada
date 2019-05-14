cdd02534aa8bf9cd45d341aba51e4010ab0b7237
LeonNiekus <leonniekusnl@gmail.com>
2019-05-14 10:12:09 +0200

    added a checksum and handle method for score update (player table update checksum), handle reloads gameview

--- src/main/java/nl/avans/sagrada/database/ChecksumDatabase.java
+++ src/main/java/nl/avans/sagrada/database/ChecksumDatabase.java
@@ -14,6 +14,7 @@ public class ChecksumDatabase {
     private PlayerController playerController;
     private String checksumPlayer;
     private String checksumChat;
+    private String checksumScore;
 
     public ChecksumDatabase(AccountController accountController,
             PlayerController playerController) {
@@ -30,22 +31,44 @@ public class ChecksumDatabase {
             public void handle() {
                 checksumPlayer();
                 checksumChat();
+                checksumScore();
             }
         };
     }
-    
+
     /**
-     * Generates a checksum for the chatline table, and checks if this checksum is different from the already existing checksum.
+     * Generates a checksum for the chatline table, and checks if this checksum is different from
+     * the already existing checksum.
      */
     private void checksumChat() {
         try {
-            ResultSet rs = dbConnection.executeQuery(new Query("CHECKSUM TABLE chatline;", "query"));
+            ResultSet rs =
+                    dbConnection.executeQuery(new Query("CHECKSUM TABLE chatline;", "query"));
             if (rs.next()) {
                 String checksumChat = rs.getString("Checksum");
                 if (!checksumChat.equals(this.checksumChat)) {
                     handleChat();
                 }
                 this.checksumChat = checksumChat;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates a checksum for the player table, and checks if this checksum is different from the
+     * already existing checksum.
+     */
+    private void checksumScore() {
+        try {
+            ResultSet rs = dbConnection.executeQuery(new Query("CHECKSUM TABLE player;", "query"));
+            if (rs.next()) {
+                String checksumScore = rs.getString("Checksum");
+                if (!checksumScore.equals(this.checksumScore)) {
+                    handleScoreUpdate();
+                }
+                this.checksumScore = checksumScore;
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -80,11 +103,22 @@ public class ChecksumDatabase {
             }
         }
     }
-    
+
     /**
-     * Reloads the gameview when an account is in a game.
+     * Reloads the gameview when an account is in a game (meant to handle chat).
      */
     private void handleChat() {
+        if (accountController.getAccount() != null) {
+            if (accountController.getAccount().getAccountStatus() == AccountStatus.GAME) {
+                playerController.viewGame();
+            }
+        }
+    }
+
+    /**
+     * Reloads the gameview when an account is in a game (meant to handle score).
+     */
+    private void handleScoreUpdate() {
         if (accountController.getAccount() != null) {
             if (accountController.getAccount().getAccountStatus() == AccountStatus.GAME) {
                 playerController.viewGame();
