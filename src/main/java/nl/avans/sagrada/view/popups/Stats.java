528ad4fd1c90d9da4dce592e551abff8598573f2
Kylian <kylianhijmans@gmail.com>
2019-05-16 10:23:50 +0200

    add account name to statsview

--- src/main/java/nl/avans/sagrada/view/popups/Stats.java
+++ src/main/java/nl/avans/sagrada/view/popups/Stats.java
@@ -18,6 +18,7 @@ public class Stats extends Popup {
     public static final int WIDTH_STATS = 630;
     public static final int HEIGHT_STATS = 340;
     private MyScene myScene;
+    private Account account;
     private final int wins;
     private final int loses;
     private final int highestScore;
@@ -28,6 +29,7 @@ public class Stats extends Popup {
     public Stats(MyScene myScene, Account account) {
         super(WIDTH_STATS, HEIGHT_STATS);
         this.myScene = myScene;
+        this.account = account;
 
         int[] winsLoses = account.getWinLoseCount();
         this.wins = winsLoses[0];
@@ -53,35 +55,41 @@ public class Stats extends Popup {
         vBox.setPadding(new Insets(20));
         vBox.setAlignment(Pos.CENTER);
 
-        Label label1 = new Label("win:lose : " + wins + ":" + loses);
-        label1.setTextFill(Color.web("#000"));
-        label1.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        Label labelName = new Label("Account: " +account.getUsername());
+        labelName.setTextFill(Color.web("#000"));
+        labelName.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        VBox.setMargin(labelName, new Insets(0, 0, 20, 0));
 
-        Label label2 = new Label("Hoogste score: " + highestScore);
-        label2.setTextFill(Color.web("#000"));
-        label2.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        Label labelWinLose = new Label("win:lose : " + wins + ":" + loses);
+        labelWinLose.setTextFill(Color.web("#000"));
+        labelWinLose.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
 
-        Label label3 = new Label("Meest gebruikte kleur: " + mostUsedColor);
-        label3.setTextFill(Color.web("#000"));
-        label3.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        Label labelHighScore = new Label("Hoogste score: " + highestScore);
+        labelHighScore.setTextFill(Color.web("#000"));
+        labelHighScore.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
 
-        Label label4 = new Label("Meest gebruikte value: " + mostUsedValue);
-        label4.setTextFill(Color.web("#000"));
-        label4.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        Label labelMostUsedColor = new Label("Meest gebruikte kleur: " + mostUsedColor);
+        labelMostUsedColor.setTextFill(Color.web("#000"));
+        labelMostUsedColor.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
 
-        Label label5 = new Label("Aantal gespeelde accounts: " + countAccounts);
-        label5.setTextFill(Color.web("#000"));
-        label5.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        Label labelMostUsedValue = new Label("Meest gebruikte value: " + mostUsedValue);
+        labelMostUsedValue.setTextFill(Color.web("#000"));
+        labelMostUsedValue.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+
+        Label labelPlayedAccounts = new Label("Aantal gespeelde accounts: " + countAccounts);
+        labelPlayedAccounts.setTextFill(Color.web("#000"));
+        labelPlayedAccounts.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
 
         Button button = new Button("Close");
         button.setOnAction(e -> myScene.removePopupPane());
         VBox.setMargin(button, new Insets(20, 0, 0, 0));
 
-        vBox.getChildren().add(label1);
-        vBox.getChildren().add(label2);
-        vBox.getChildren().add(label3);
-        vBox.getChildren().add(label4);
-        vBox.getChildren().add(label5);
+        vBox.getChildren().add(labelName);
+        vBox.getChildren().add(labelWinLose);
+        vBox.getChildren().add(labelHighScore);
+        vBox.getChildren().add(labelMostUsedColor);
+        vBox.getChildren().add(labelMostUsedValue);
+        vBox.getChildren().add(labelPlayedAccounts);
         vBox.getChildren().add(button);
         getChildren().add(vBox);
     }
