c13c53be7aca0c0d5f72b4b96a3f81010f7f2e8a
Jelles <jelles.duin@gmail.com>
2019-05-17 23:21:13 +0200

    toolcard finished

--- src/main/java/nl/avans/sagrada/model/toolcard/ToolCardEglomiseBorstel.java
+++ src/main/java/nl/avans/sagrada/model/toolcard/ToolCardEglomiseBorstel.java
@@ -1,18 +1,48 @@
 package nl.avans.sagrada.model.toolcard;
 
 import javafx.scene.input.MouseEvent;
+import nl.avans.sagrada.dao.PlayerFrameFieldDao;
 import nl.avans.sagrada.model.GameDie;
 import nl.avans.sagrada.model.PatternCard;
+import nl.avans.sagrada.model.PatternCardField;
+import nl.avans.sagrada.model.Player;
+import nl.avans.sagrada.view.PatternCardFieldView;
 
 public class ToolCardEglomiseBorstel extends ToolCard {
-    
+
     public ToolCardEglomiseBorstel(int id, String name, int seqnr, String description) {
         super(id, name, seqnr, description);
     }
 
     @Override
     public PatternCard handleDrag(MouseEvent event, GameDie die) {
-        // TODO Auto-generated method stub
+        PlayerFrameFieldDao playerFrameFieldDao = new PlayerFrameFieldDao();
+        PatternCardFieldView patternCardFieldView = (PatternCardFieldView) event.getTarget();
+        PatternCardField targetField = patternCardFieldView.getPatternCardField();
+        PatternCardField removeDieField = die.getPatternCardField();
+        PatternCard patternCard = targetField.getPatternCard();
+        Player player = patternCard.getPlayer();
+
+        String dieColor = die.getColor();
+        int dieEyes = die.getEyes();
+        int targetValue = targetField.getValue();
+
+        if (!targetField.hasDie() && patternCard.checkSidesValue(targetField, die.getEyes(), true)
+                && patternCard.isNextToDie(targetField)) {
+            if (targetField.hasValue()) {
+                if (targetField.getValue() != die.getEyes()) {
+                    return null;
+                }
+            }
+            playerFrameFieldDao.removeDie(die, removeDieField, player);
+            removeDieField.setDie(null);
+            die.setPatternCardField(targetField);
+            targetField.setDie(die);
+            playerFrameFieldDao.addDieToField(die, targetField, player);
+
+
+            return patternCard;
+        }
         return null;
     }
 
