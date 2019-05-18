cb9740cf0b9d6bddebd6e66603b562f620062eab
LeonNiekus <leonniekusnl@gmail.com>
2019-05-18 19:34:22 +0200

    added some checks

--- src/main/java/nl/avans/sagrada/model/toolcard/ToolCardSchuurBlok.java
+++ src/main/java/nl/avans/sagrada/model/toolcard/ToolCardSchuurBlok.java
@@ -25,53 +25,57 @@ public class ToolCardSchuurBlok extends ToolCard {
             PatternCardField patternCardField = patternCardView.getPatternCardField();
             PatternCard patternCard = patternCardField.getPatternCard();
             Player player = patternCard.getPlayer();
-            
-            int eyes = die.getEyes();
-            switch (eyes) {
-                case 1:
-                    die.setEyes(6);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                case 2:
-                    die.setEyes(5);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                case 3:
-                    die.setEyes(4);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                case 4:
-                    die.setEyes(3);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                case 5:
-                    die.setEyes(2);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                case 6:
-                    die.setEyes(1);
-                    gameDieDao.updateDieEyes(player.getGame(), die);
-                    die.setPatternCardField(patternCardField);
-                    patternCardField.setDie(die);
-                    playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                    return patternCard;
-                default:
-                    return null;
+
+            if (patternCardField.hasDie() == false && patternCardField.canPlaceDieByAttributes(die)
+                    && patternCard.checkSidesColor(patternCardField, die.getColor(), true)
+                    && patternCard.checkSidesValue(patternCardField, die.getEyes(), true)) {
+                int eyes = die.getEyes();
+                switch (eyes) {
+                    case 1:
+                        die.setEyes(6);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    case 2:
+                        die.setEyes(5);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    case 3:
+                        die.setEyes(4);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    case 4:
+                        die.setEyes(3);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    case 5:
+                        die.setEyes(2);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    case 6:
+                        die.setEyes(1);
+                        gameDieDao.updateDieEyes(player.getGame(), die);
+                        die.setPatternCardField(patternCardField);
+                        patternCardField.setDie(die);
+                        playerFrameFieldDao.addDieToField(die, patternCardField, player);
+                        return patternCard;
+                    default:
+                        return null;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
