702cd5fb80bc0752f4ecaf0f774cc8e376a8031a
Peter Romijn <peter@samebestdevelopment.nl>
2019-05-27 19:06:20 +0200

    Added docs

--- src/main/java/nl/avans/sagrada/task/CancelGameTask.java
+++ src/main/java/nl/avans/sagrada/task/CancelGameTask.java
@@ -5,6 +5,10 @@ import nl.avans.sagrada.model.Game;
 public class CancelGameTask implements Runnable {
     private Game game;
     
+    /**
+     * Constructor for cancel game task
+     * @param game
+     */
     public CancelGameTask(Game game) {
         this.game = game;
     }
