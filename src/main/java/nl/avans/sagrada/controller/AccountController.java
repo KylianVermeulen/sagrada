19a3c8866f35cb206a66fe9a2532a89580547e3b
Peter Romijn <peterromijn@icloud.com>
2019-05-27 11:30:28 +0200

    fix: cancel game setupview (#120)

--- src/main/java/nl/avans/sagrada/controller/AccountController.java
+++ src/main/java/nl/avans/sagrada/controller/AccountController.java
@@ -191,7 +191,6 @@ public class AccountController {
         game.assignRandomToolCards();
         game.assignRandomPublicObjectiveCards();
         game.addDice();
-        game.setPlayers(gameDao.getPlayersOfGame(game));
 
         int playerId = playerDao.getNextPlayerId();
         Player player = new Player();
@@ -203,6 +202,8 @@ public class AccountController {
         player.setGame(game);
         player.setPrivateObjectivecardColor(game.getRandomAvailablePrivateColor());
         playerDao.addPlayer(player);
+        
+        game.setPlayers(gameDao.getPlayersOfGame(game));
 
         game.setTurnPlayer(player);
         gameDao.updateGame(game);
