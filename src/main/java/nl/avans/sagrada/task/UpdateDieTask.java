df3d4d54de109833c80864ad89c6e2cbcd489af4
Peter Romijn <peter@samebestdevelopment.nl>
2019-05-27 19:25:48 +0200

    Added treading for placedie

---
+++ src/main/java/nl/avans/sagrada/task/UpdateDieTask.java
@@ -0,0 +1,26 @@
+package nl.avans.sagrada.task;
+
+import nl.avans.sagrada.dao.GameDieDao;
+import nl.avans.sagrada.model.Game;
+import nl.avans.sagrada.model.GameDie;
+
+public class UpdateDieTask implements Runnable {
+    private Game game;
+    private GameDie gameDie;
+    
+    /**
+     * Constructor for updateDieTask
+     * @param game
+     * @param gameDie
+     */
+    public UpdateDieTask(Game game, GameDie gameDie) {
+        this.game = game;
+        this.gameDie = gameDie;
+    }
+
+    @Override
+    public void run() {
+        new GameDieDao().updateDie(game, gameDie);
+    }
+
+}
