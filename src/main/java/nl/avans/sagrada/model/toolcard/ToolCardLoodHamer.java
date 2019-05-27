3e9e7fb5f26d9420809b8b287240410d404aa993
LeonNiekus <leonniekusnl@gmail.com>
2019-05-27 12:33:20 +0200

    can place die after use

--- src/main/java/nl/avans/sagrada/model/toolcard/ToolCardLoodHamer.java
+++ src/main/java/nl/avans/sagrada/model/toolcard/ToolCardLoodHamer.java
@@ -3,6 +3,7 @@ package nl.avans.sagrada.model.toolcard;
 import javafx.scene.input.MouseEvent;
 import nl.avans.sagrada.controller.PlayerController;
 import nl.avans.sagrada.dao.PlayerDao;
+import nl.avans.sagrada.dao.PlayerFrameFieldDao;
 import nl.avans.sagrada.model.GameDie;
 import nl.avans.sagrada.model.PatternCard;
 import nl.avans.sagrada.model.PatternCardField;
@@ -21,6 +22,27 @@ public class ToolCardLoodHamer extends ToolCard {
 
     @Override
     public PatternCard handleDrag(MouseEvent event, GameDie die) {
+        try {
+            PlayerFrameFieldDao playerFrameFieldDao = new PlayerFrameFieldDao();
+            PatternCardFieldView patternCardView = (PatternCardFieldView) event.getTarget();
+
+            PatternCardField patternCardField = patternCardView.getPatternCardField();
+            PatternCard patternCard = patternCardField.getPatternCard();
+            Player player = patternCard.getPlayer();
+
+            if (patternCardField.hasDie() == false && patternCardField
+                    .canPlaceDieByAttributes(die)
+                    && patternCard.checkSidesColor(patternCardField, die.getColor(), true)
+                    && patternCard.checkSidesValue(patternCardField, die.getEyes(), true)) {
+                die.setPatternCardField(patternCardField);
+                patternCardField.setDie(die);
+                playerFrameFieldDao.addDieToField(die, patternCardField, player);
+
+                return patternCard;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
