b3d594640e2caf4a02aa18d588c06d40a7902afe
Kylian <kylian@kylianvermeulen.nl>
2019-05-29 09:30:01 +0200

    hotfix: rs close on update statements (#131)

--- src/main/java/nl/avans/sagrada/dao/GameDao.java
+++ src/main/java/nl/avans/sagrada/dao/GameDao.java
@@ -59,7 +59,6 @@ public class GameDao {
                             new QueryParameter(QueryParameter.INT, turnPlayerId),
                             new QueryParameter(QueryParameter.INT, game.getId())
             );
-            rs.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -79,7 +78,6 @@ public class GameDao {
                             new QueryParameter(QueryParameter.INT, game.getId()),
                             new QueryParameter(QueryParameter.TIMESTAMP, game.getCreationDate())
             );
-            rs.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
