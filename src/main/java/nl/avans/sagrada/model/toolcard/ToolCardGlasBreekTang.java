f9e80a39b15c546c9159757507e54cf994ff961f
Kylian <kylian@kylianvermeulen.nl>
2019-05-31 17:50:37 +0200

    add threading toolcard addDieToField

--- src/main/java/nl/avans/sagrada/model/toolcard/ToolCardGlasBreekTang.java
+++ src/main/java/nl/avans/sagrada/model/toolcard/ToolCardGlasBreekTang.java
@@ -7,6 +7,7 @@ import nl.avans.sagrada.model.GameDie;
 import nl.avans.sagrada.model.PatternCard;
 import nl.avans.sagrada.model.PatternCardField;
 import nl.avans.sagrada.model.Player;
+import nl.avans.sagrada.task.UpdatePlayerFrameFieldTask;
 import nl.avans.sagrada.view.PatternCardFieldView;
 
 /**
@@ -31,8 +32,12 @@ public class ToolCardGlasBreekTang extends ToolCard {
             if (patternCardField.placeDie(die) && die.getPatternCardField() != null) {
                 die.setPatternCardField(patternCardField);
                 patternCardField.setDie(die);
-                playerFrameFieldDao.addDieToField(die, patternCardField, player);
-                
+                UpdatePlayerFrameFieldTask updatePlayerFrameFieldTask = new UpdatePlayerFrameFieldTask(die, patternCardField, player);
+                Thread thread = new Thread(updatePlayerFrameFieldTask);
+                thread.setName("Update Player Frame Field");
+                thread.setDaemon(true);
+                thread.start();
+
                 setIsDone(true);
                 return patternCard;
             }
