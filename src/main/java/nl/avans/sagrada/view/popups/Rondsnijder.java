43d2c5a7dcf4d0889a49d7ae4505a0ca21d1369d
Kylian <kylian@kylianvermeulen.nl>
2019-05-31 23:14:13 +0200

    WIP

---
+++ src/main/java/nl/avans/sagrada/view/popups/Rondsnijder.java
@@ -0,0 +1,140 @@
+package nl.avans.sagrada.view.popups;
+
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import nl.avans.sagrada.controller.PlayerController;
+import nl.avans.sagrada.dao.GameDieDao;
+import nl.avans.sagrada.model.Game;
+import nl.avans.sagrada.model.GameDie;
+import nl.avans.sagrada.model.toolcard.ToolCard;
+import nl.avans.sagrada.view.DieView;
+import nl.avans.sagrada.view.MyScene;
+
+public class Rondsnijder extends Popup {
+    public static final int WIDTH_FLUXBORSTEL = 630;
+    public static final int HEIGHT_FLUXBORSTEL = 170;
+
+    private MyScene myScene;
+    private HBox diePane;
+    private VBox rootPane;
+    private Game game;
+    private Text textTop;
+    private Text textBot;
+    private ToolCard toolCard;
+    private PlayerController playerController;
+    private ArrayList<DieView> dieViews;
+    private ArrayList<GameDie> gameDice;
+
+    /**
+     * Full constructor
+     *
+     * @param myScene MyScene
+     * @param game Game
+     * @param playerController PlayerController
+     */
+    public Rondsnijder(MyScene myScene, Game game, PlayerController playerController,
+            ToolCard toolCard) {
+        super(WIDTH_FLUXBORSTEL, HEIGHT_FLUXBORSTEL);
+        this.myScene = myScene;
+        this.game = game;
+        this.playerController = playerController;
+        this.toolCard = toolCard;
+
+        setMaxSize(USE_PREF_SIZE, USE_PREF_SIZE);
+        setBackground(new Background(
+                new BackgroundFill(Color.web("#fff"), new CornerRadii(26), null)));
+        this.render();
+    }
+
+    @Override
+    public void render() {
+        dieViews = new ArrayList<DieView>();
+        gameDice = new ArrayList<GameDie>();
+
+        textTop = new Text();
+        textBot = new Text();
+        diePane = new HBox();
+        diePane.setAlignment(Pos.CENTER);
+        rootPane = new VBox();
+        rootPane.setAlignment(Pos.CENTER);
+        rootPane.setPrefSize(super.getwidth(), super.getheight());
+        
+        int index = 0;
+        for (GameDie gameDie : game.getRoundDice()) {
+            gameDie.setInFirstTurn(playerController.getPlayer().isFirstTurn());
+            int i = index;
+            Pane paddingPane = new Pane();
+            DieView dieView = new DieView(gameDie, playerController);
+            dieViews.add(dieView);
+            gameDice.add(gameDie);
+            dieView.resize(25, 25);
+            paddingPane.setPadding(new Insets(20));
+            dieView.render();
+            dieView.setOnMouseClicked(e -> {
+                rerollDice(i);
+            });
+            dieView.disableDrag();
+            paddingPane.getChildren().add(dieView);
+            diePane.getChildren().add(paddingPane);
+            index++;
+        }
+        textTop.setText("Kies een dobbelsteen die je wil omruilen.");
+        textBot.setText("Als je klikt heb je gekozen dus denk eerst na voordat je klikt!\n");
+        textTop.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        textBot.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        rootPane.getChildren().addAll(textTop, textBot, diePane);
+        this.getChildren().add(rootPane);
+    }
+
+    /**
+     * Rerolls the dice that is clicked.
+     *
+     * @param index int
+     */
+    private void rerollDice(int index) {
+        GameDieDao gameDieDao = new GameDieDao();
+        GameDie gameDie = gameDice.get(index);
+        int newEyes = new Random().nextInt(6) + 1;
+        gameDie.setEyes(newEyes);
+        gameDieDao.updateDieEyes(game, gameDie);
+        showDie(gameDie);
+    }
+
+    /**
+     * Shows the chosen die on the popup so you can place it.
+     *
+     * @param gameDie GameDie
+     */
+    private void showDie(GameDie gameDie) {
+        gameDie.enablePopupdie();
+        gameDie.setRound(playerController.getPlayer().getGame().getRound());
+        DieView dieView = new DieView(gameDie, playerController);
+        dieView.render();
+        textTop.setText("Dit is de enigste dobbelsteen die je kunt plaatsen.");
+        textBot.setText(
+                "Sleep de steen naar de plek waar je hem wil plaatsen anders klik je op de knop.");
+        diePane.getChildren().clear();
+        rootPane.getChildren().clear();
+        diePane.getChildren().add(dieView);
+        Button button = new Button("Eindid beurt");
+        
+        button.setOnAction(e -> {
+            myScene.removePopupPane();
+            playerController.actionPass();
+        });
+        rootPane.getChildren().addAll(textTop, textBot, diePane, button);
+    }
+}
