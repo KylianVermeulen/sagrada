61f6cd89cb8966f9b025fea94498d903b3dc1462
Kylian <kylian@kylianvermeulen.nl>
2019-06-03 11:05:48 +0200

    feat: toolcard rondsnijder (#185)

--- src/main/java/nl/avans/sagrada/model/toolcard/ToolCardRondSnijder.java
+++ src/main/java/nl/avans/sagrada/model/toolcard/ToolCardRondSnijder.java
@@ -4,12 +4,17 @@ import javafx.scene.input.MouseEvent;
 import nl.avans.sagrada.controller.PlayerController;
 import nl.avans.sagrada.model.GameDie;
 import nl.avans.sagrada.model.PatternCard;
+import nl.avans.sagrada.model.PatternCardField;
+import nl.avans.sagrada.model.Player;
+import nl.avans.sagrada.task.UpdatePlayerFrameFieldTask;
+import nl.avans.sagrada.view.PatternCardFieldView;
 
 /**
  * Nadat je een dobbelsteen kiest, mag je de gekozen dobbeslteen wisselen met een dobbelsteen op het
  * Rondespoor.
  */
 public class ToolCardRondSnijder extends ToolCard {
+    private PlayerController playerController;
 
     public ToolCardRondSnijder(int id, String name, int seqnr, String description) {
         super(id, name, seqnr, description);
@@ -17,11 +22,37 @@ public class ToolCardRondSnijder extends ToolCard {
 
     @Override
     public PatternCard handleDrag(MouseEvent event, GameDie die) {
+        if (die.isPopUpDie()) {
+            if (event.getTarget() instanceof PatternCardFieldView) {
+                PatternCardFieldView patternCardFieldView = (PatternCardFieldView) event
+                        .getTarget();
+                PatternCardField targetField = patternCardFieldView.getPatternCardField();
+                PatternCard patternCard = targetField.getPatternCard();
+                Player player = patternCard.getPlayer();
+
+                if (targetField.placeDie(die)) {
+                    playerController.removePopupPane();
+                    die.setInFirstTurn(player.isFirstTurn());
+                    UpdatePlayerFrameFieldTask updatePlayerFrameFieldTask = new UpdatePlayerFrameFieldTask(die, targetField, player);
+                    Thread thread = new Thread(updatePlayerFrameFieldTask);
+                    thread.setName("Update Player Frame Field");
+                    thread.setDaemon(true);
+                    thread.start();
+                    die.setPatternCardField(targetField);
+                    targetField.setDie(die);
+                    die.setIsOnOfferTable(false);
+                    setIsDone(true);
+                    return patternCard;
+                }
+            }
+            return null;
+        }
         return null;
     }
 
     @Override
     public boolean hasRequirementsToRun(PlayerController playerController) {
-        return true;
+        this.playerController = playerController;
+        return playerController.getPlayer().getGame().getRound() > 1;
     }
 }
---
+++ src/main/java/nl/avans/sagrada/view/popups/Rondsnijder.java
@@ -0,0 +1,210 @@
+package nl.avans.sagrada.view.popups;
+
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import nl.avans.sagrada.controller.PlayerController;
+import nl.avans.sagrada.dao.GameDieDao;
+import nl.avans.sagrada.model.Game;
+import nl.avans.sagrada.model.GameDie;
+import nl.avans.sagrada.model.toolcard.ToolCard;
+import nl.avans.sagrada.view.DieView;
+import nl.avans.sagrada.view.MyScene;
+
+public class Rondsnijder extends Popup {
+    public static final int WIDTH_FLUXBORSTEL = 630;
+    public static final int HEIGHT_FLUXBORSTEL = 170;
+    private MyScene myScene;
+    private HBox diePane;
+    private HBox dieTrackPane;
+    private VBox rootPane;
+    private Game game;
+    private Text textTop;
+    private Text textBot;
+    private ToolCard toolCard;
+    private PlayerController playerController;
+    private ArrayList<DieView> dieViews;
+    private ArrayList<GameDie> gameDice;
+    private ArrayList<GameDie> gameTrackDice;
+    private GameDie die1;
+    private GameDie die2;
+
+    /**
+     * Full constructor
+     *
+     * @param myScene MyScene
+     * @param game Game
+     * @param playerController PlayerController
+     */
+    public Rondsnijder(MyScene myScene, Game game, PlayerController playerController,
+            ToolCard toolCard) {
+        super(WIDTH_FLUXBORSTEL, HEIGHT_FLUXBORSTEL);
+        this.myScene = myScene;
+        this.game = game;
+        this.playerController = playerController;
+        this.toolCard = toolCard;
+
+        setMaxSize(USE_PREF_SIZE, USE_PREF_SIZE);
+        setBackground(new Background(
+                new BackgroundFill(Color.web("#fff"), new CornerRadii(26), null)));
+        this.render();
+    }
+
+    @Override
+    public void render() {
+        dieViews = new ArrayList<>();
+        gameDice = new ArrayList<>();
+        gameTrackDice = new ArrayList<>();
+
+        textTop = new Text();
+        textBot = new Text();
+        diePane = new HBox();
+        diePane.setAlignment(Pos.CENTER);
+        dieTrackPane = new HBox();
+        dieTrackPane.setAlignment(Pos.BOTTOM_CENTER);
+        rootPane = new VBox();
+        rootPane.setAlignment(Pos.CENTER);
+        rootPane.setPrefSize(super.getwidth(), super.getheight());
+
+        showRoundDice();
+        showTrackDice();
+
+        textTop.setText("Kies 2 dobbelsteen die je wil omruilen.");
+        textBot.setText("1 op de eerste rij en 1 op de laatse rij!\n");
+        textTop.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        textBot.setFont(Font.font("sans-serif", FontWeight.MEDIUM, 18));
+        rootPane.getChildren().addAll(textTop, textBot, diePane, dieTrackPane);
+        this.getChildren().add(rootPane);
+    }
+
+    /**
+     * Shows the dice of the current round in the popup.
+     */
+    private void showRoundDice() {
+        int index = 0;
+        for (GameDie gameDie : game.getRoundDice()) {
+            gameDie.setInFirstTurn(playerController.getPlayer().isFirstTurn());
+            int i = index;
+            Pane paddingPane = new Pane();
+            DieView dieView = new DieView(gameDie, playerController);
+            dieViews.add(dieView);
+            gameDice.add(gameDie);
+            dieView.resize(25, 25);
+            paddingPane.setPadding(new Insets(20));
+            dieView.render();
+            dieView.setOnMouseClicked(e -> {
+                selectDice(i);
+            });
+            dieView.disableDrag();
+            paddingPane.getChildren().add(dieView);
+            diePane.getChildren().add(paddingPane);
+            index++;
+        }
+    }
+
+    /**
+     * Shows the dice of the current round track in the game.
+     */
+    private void showTrackDice() {
+        int index = 0;
+        for (GameDie gameDie : game.getRoundTrackDice()) {
+            gameDie.setInFirstTurn(playerController.getPlayer().isFirstTurn());
+            int i = index;
+            Pane paddingPane = new Pane();
+            DieView dieView = new DieView(gameDie, playerController);
+            dieViews.add(dieView);
+            gameTrackDice.add(gameDie);
+            dieView.resize(25, 25);
+            paddingPane.setPadding(new Insets(20));
+            dieView.render();
+            dieView.setOnMouseClicked(e -> {
+                selectTrackDice(i);
+            });
+            dieView.disableDrag();
+            paddingPane.getChildren().add(dieView);
+            dieTrackPane.getChildren().add(paddingPane);
+            index++;
+        }
+    }
+
+    /**
+     * Select round dice, continue if track dice is selected.
+     *
+     * @param index int
+     */
+    private void selectDice(int index) {
+        GameDie gameDie = gameDice.get(index);
+        die1 = gameDie;
+        if (die2 != null) {
+            handle();
+        }
+    }
+
+    /**
+     * Select track dice, continue if round dice is selected.
+     *
+     * @param index int
+     */
+    private void selectTrackDice(int index) {
+        GameDie gameDie = gameTrackDice.get(index);
+        die2 = gameDie;
+        if (die1 != null) {
+            handle();
+        }
+    }
+
+    /**
+     * Handle the dice swap.
+     */
+    private void handle() {
+        GameDieDao gameDieDao = new GameDieDao();
+        final int die1Round = die1.getRound();
+        final int die2Round = die2.getRound();
+        die1.setIsOnOfferTable(false);
+        die1.setOnRoundTrack(true);
+        die1.setRound(die2Round);
+        die2.setRound(die1Round);
+        die2.setIsOnOfferTable(true);
+        die2.setOnRoundTrack(false);
+        die2.setInFirstTurn(playerController.getPlayer().isFirstTurn());
+        gameDieDao.updateDie(game, die1);
+        gameDieDao.updateDie(game, die2);
+        showDie(die2);
+    }
+
+    /**
+     * Shows the chosen die on the popup so you can place it.
+     *
+     * @param gameDie GameDie
+     */
+    private void showDie(GameDie gameDie) {
+        gameDie.enablePopupdie();
+        gameDie.setRound(playerController.getPlayer().getGame().getRound());
+        DieView dieView = new DieView(gameDie, playerController);
+        dieView.render();
+        textTop.setText("Dit is de enigste dobbelsteen die je kunt plaatsen.");
+        textBot.setText(
+                "Sleep de steen naar de plek waar je hem wil plaatsen anders klik je op de knop.");
+        diePane.getChildren().clear();
+        rootPane.getChildren().clear();
+        diePane.getChildren().add(dieView);
+        Button button = new Button("Eindig beurt");
+        button.setOnAction(e -> {
+            myScene.removePopupPane();
+            playerController.actionPass();
+        });
+        rootPane.getChildren().addAll(textTop, textBot, diePane, button);
+    }
+}
